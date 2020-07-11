a33e83e3fe01aabb6876d0151cfb5758b1378a05
Vitalii Pepko <vitaliipepko@gmail.com>
2020-07-11 15:09:13 +0300

    change class Post

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-	blog_title = models.CharField(max_length = 150)
-	blog_date = models.DateTimeField(auto_now = True)
-	blog_text = models.TextField(max_length = 10000)
-	blog_image = models.ImageField(upload_to = 'blog_images/')
+	title = models.CharField(max_length = 150)
+	date = models.DateTimeField(auto_now = True)
+	text = models.TextField(max_length = 10000)
+	image = models.ImageField(upload_to = 'blog_images/')
